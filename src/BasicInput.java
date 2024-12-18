be0eb2f69d90426e98dd6825c11719a4ec9ecf1d
Sanjeev Kumar <sanjeev.kumar@ksolves.com>
2024-12-18 08:34:09 +0530

    First Commit

---
+++ src/BasicInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BasicInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int a = sc.nextInt();
+            String b = sc.next();
+            System.out.println("result");
+        }
+    }
+}
