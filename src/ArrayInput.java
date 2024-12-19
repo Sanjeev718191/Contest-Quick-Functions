13e723ce701e8a9379a006bc601f76be96ad70d8
Sanjeev Kumar <sanjeev718191@gmail.com>
2024-12-19 08:32:06 +0530

    Array input

--- src/ArrayInput.java
+++ src/ArrayInput.java
@@ -1,5 +1,18 @@
+
+import java.util.Scanner;
+
 public class ArrayInput {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t -- > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int res = arr[0];
+            System.out.println(res);
+        }
     }
 }
